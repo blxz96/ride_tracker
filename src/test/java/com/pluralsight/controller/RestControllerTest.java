20538e348a42bb6dea7892cf05d9a3c208f78ded
Bryan Leow Xuan Zhen <blxz96@gmail.com>
2021-10-17 16:56:10 +0800

    -Completed Module 4: Creating Records in the Database

--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -13,7 +13,17 @@ import org.junit.Test;
 
 public class RestControllerTest {
 
-	@Test(timeout=3000)
+	@Test(timeout=30000)
+	public void testCreateRide(){
+		RestTemplate restTemplate = new RestTemplate();
+		Ride ride = new Ride();
+		ride.setName("The Glorious Path");
+		ride.setDuration(13);
+		restTemplate.put("http://localhost:8080/ride_tracker/ride",ride);
+	}
+
+
+	@Test(timeout=30000)
 	public void testGetRides() {
 		RestTemplate restTemplate = new RestTemplate();
 
